2563d74b6c315aab71038c7ffa6a92796b6b46b9
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-01-10 15:26:11 +0200

    fix

--- src/components/Block/Block.jsx
+++ src/components/Block/Block.jsx
@@ -1,5 +1,6 @@
 import css from './Block.module.css';
 import give_arrow from '../../images/give_arrow.png';
+import get_arrow from '../../images/get_arrow.png';
 
 const fiatCurrencies = [
   {
@@ -42,15 +43,17 @@ export const Block = ({
   value,
   name,
   img,
-  onChangeCurrency,
-  currency,
+  onChangeCurrencyFrom,
+  onChangeCurrencyTo,
+  currencyFrom,
+  currencyTo,
 }) => {
   return (
     <>
       <div className={css.give__currency}>
         <div className={css.crypto__thumb}>
           <div className={css.title}>
-            ПОЛУЧАЕТЕ:
+            ОТДАЁТЕ:
             <img src={give_arrow} width="24" height="28" alt="" />
           </div>
 
@@ -58,8 +61,23 @@ export const Block = ({
           {cryptoCurrencies.map(({ key, value, img, name }) => (
             <li
               key={value}
-              onClick={() => onChangeCurrency(value)}
-              className={value === currency ? css.active : {}}
+              onClick={() => onChangeCurrencyFrom(value)}
+              className={value === currencyFrom ? css.active : {}}
+            >
+              <p className={css.cryptocurrency__type}>
+                <span className={css.currency__img}>
+                  <img src={img} width="36" height="36" alt="" />
+                </span>
+                {name}
+              </p>
+            </li>
+          ))}
+          {/* CONDITION */}
+          {fiatCurrencies.map(({ key, value, img, name }) => (
+            <li
+              key={value}
+              onClick={() => onChangeCurrencyFrom(value)}
+              className={value === currencyFrom ? css.active : {}}
             >
               <p className={css.cryptocurrency__type}>
                 <span className={css.currency__img}>
@@ -70,21 +88,57 @@ export const Block = ({
             </li>
           ))}
 
+          <ul className={css.crypto__currencies__list}></ul>
+        </div>
+      </div>
+
+      <div className={css.get__currency}>
+        <div className={css.crypto__thumb}>
+          <div className={css.title}>
+            ПОЛУЧАЕТЕ:
+            <img src={get_arrow} width="24" height="28" alt="" />
+          </div>
+          {currencyFrom === 'BTC' ||
+          currencyFrom === 'ETH' ||
+          currencyFrom === 'tether' ? (
+            <>
+              {fiatCurrencies.map(({ key, value, img, name }) => (
+                <li
+                  key={value}
+                  onClick={() => onChangeCurrencyTo(value)}
+                  className={value === currencyTo ? css.active : {}}
+                >
+                  <p className={css.cryptocurrency__type}>
+                    <span className={css.currency__img}>
+                      <img src={img} width="36" height="36" alt="" />
+                    </span>
+                    {name}
+                  </p>
+                </li>
+              ))}
+            </>
+          ) : (
+            <>
+              {cryptoCurrencies.map(({ key, value, img, name }) => (
+                <li
+                  key={value}
+                  onClick={() => onChangeCurrencyTo(value)}
+                  className={value === currencyTo ? css.active : {}}
+                >
+                  <p className={css.cryptocurrency__type}>
+                    <span className={css.currency__img}>
+                      <img src={img} width="36" height="36" alt="" />
+                    </span>
+                    {name}
+                  </p>
+                </li>
+              ))}
+            </>
+          )}
           {/* CONDITION */}
-          {fiatCurrencies.map(({ key, value, img, name }) => (
-            <li
-              key={value}
-              onClick={() => onChangeCurrency(value)}
-              className={value === currency ? css.active : {}}
-            >
-              <p className={css.cryptocurrency__type}>
-                <span className={css.currency__img}>
-                  <img src={img} width="36" height="36" alt="" />
-                </span>
-                {name}
-              </p>
-            </li>
-          ))}
+
+          {/* CONDITION */}
+
           <ul className={css.crypto__currencies__list}></ul>
         </div>
       </div>
--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverter/CurrencyConverter.jsx
@@ -27,21 +27,183 @@ import ConvertTo from 'components/ConvertTo';
 // const API_KEY = 'b9474da3d7b64496bd5be9a5519204ba';
 
 export function CurrencyConverter() {
-  const [fromCurrency, setFromCurrency] = useState('btc');
-  const [toCurrency, setToCurrency] = useState('usd');
+  const [fromCurrency, setFromCurrency] = useState('BTC');
+  const [toCurrency, setToCurrency] = useState('USD');
   const [fromPrice, setFromPrice] = useState('');
   const [toPrice, setToPrice] = useState('');
   const [rates, setRates] = useState();
 
   useEffect(() => {
-    fetch(
-      `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`
-    ).then(res =>
-      res.json().then(json => {
-        setRates(json.result);
-        console.log(json.result);
-      })
-    );
+    if (fromCurrency === 'BTC' && toCurrency === 'USD') {
+      fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT').then(
+        res =>
+          res.json().then(json => {
+            setRates(json.price);
+            console.log(json.price);
+          })
+      );
+    }
+    if (fromCurrency === 'BTC' && toCurrency === 'EUR') {
+      fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCEUR').then(res =>
+        res.json().then(json => {
+          setRates(json.price);
+          console.log(json.price);
+        })
+      );
+    }
+    if (fromCurrency === 'BTC' && toCurrency === 'PLN') {
+      fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCPLN').then(res =>
+        res.json().then(json => {
+          setRates(json.price);
+          console.log(json.price);
+        })
+      );
+    }
+    if (fromCurrency === 'USD' && toCurrency === 'BTC') {
+      fetch(
+        `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.result);
+          console.log(json.result);
+        })
+      );
+    }
+    if (fromCurrency === 'EUR' && toCurrency === 'BTC') {
+      fetch(
+        `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.result);
+          console.log(json.result);
+        })
+      );
+    }
+    if (fromCurrency === 'PLN' && toCurrency === 'BTC') {
+      fetch(
+        `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.result);
+          console.log(json.result);
+        })
+      );
+    }
+    if (fromCurrency === 'ETH' && toCurrency === 'USD') {
+      fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT').then(
+        res =>
+          res.json().then(json => {
+            setRates(json.price);
+            console.log(json.price);
+          })
+      );
+    }
+    if (fromCurrency === 'ETH' && toCurrency === 'EUR') {
+      fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHEUR').then(res =>
+        res.json().then(json => {
+          setRates(json.price);
+          console.log(json.price);
+        })
+      );
+    }
+    if (fromCurrency === 'ETH' && toCurrency === 'PLN') {
+      fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHPLN').then(res =>
+        res.json().then(json => {
+          setRates(json.price);
+          console.log(json.price);
+        })
+      );
+    }
+    if (fromCurrency === 'USD' && toCurrency === 'ETH') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=ETH'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.ETH);
+          console.log(json.ETH);
+        })
+      );
+    }
+    if (fromCurrency === 'EUR' && toCurrency === 'ETH') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=ETH'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.ETH);
+          console.log(json.ETH);
+        })
+      );
+    }
+    if (fromCurrency === 'PLN' && toCurrency === 'ETH') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=ETH'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.ETH);
+          console.log(json.ETH);
+        })
+      );
+    }
+    if (fromCurrency === 'tether' && toCurrency === 'USD') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=USD'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.USD);
+          console.log(json.USD);
+        })
+      );
+    }
+    if (fromCurrency === 'tether' && toCurrency === 'EUR') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=EUR'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.EUR);
+          console.log(json.EUR);
+        })
+      );
+    }
+    if (fromCurrency === 'tether' && toCurrency === 'PLN') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=PLN'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.PLN);
+          console.log(json.PLN);
+        })
+      );
+    }
+    if (fromCurrency === 'USD' && toCurrency === 'tether') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=USDT'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.USDT);
+          console.log(json.USDT);
+        })
+      );
+    }
+    if (fromCurrency === 'EUR' && toCurrency === 'tether') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=USDT'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.USDT);
+          console.log(json.USDT);
+        })
+      );
+    }
+    if (fromCurrency === 'PLN' && toCurrency === 'tether') {
+      fetch(
+        'https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=USDT'
+      ).then(res =>
+        res.json().then(json => {
+          setRates(json.USDT);
+          console.log(json.USDT);
+        })
+      );
+    }
   }, [fromCurrency, toCurrency]);
 
   const onChangeFromPrice = value => {
@@ -59,10 +221,14 @@ export function CurrencyConverter() {
 
   return (
     <div className={css.wrapper}>
-      <Block currency={fromCurrency} onChangeCurrency={setFromCurrency} />
-      <BlockGet currency={toCurrency} onChangeCurrency={setToCurrency} />
+      <Block
+        currencyFrom={fromCurrency}
+        onChangeCurrencyFrom={setFromCurrency}
+        currencyTo={toCurrency}
+        onChangeCurrencyTo={setToCurrency}
+      />
       <div className={css.exchange__rate__thumb}>
-        <div className={css.title}>Курс на данный момент: {rates}</div>
+        <p className={css.title}>Курс на данный момент: {rates}</p>
         <ul className={css.exchange__rate}>
           <ConvertFrom value={fromPrice} onChangeValue={onChangeFromPrice} />
           <ConvertTo value={toPrice} onChangeValue={onChangeToPrice} />
