ca3c80f2748ca47bf7b15d9d6b2e6f2491e4182f
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-01-10 15:26:34 +0200

    fix

--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverter/CurrencyConverter.jsx
@@ -2,29 +2,8 @@ import css from './CurrencyConverter.module.css';
 import { useState, useEffect } from 'react';
 
 import Block from '../Block';
-import BlockGet from 'components/BlockGet';
 import ConvertFrom from '../ConvertFrom';
 import ConvertTo from 'components/ConvertTo';
-
-// До 1.000$ - 2, 5 %
-// Від 1.000$ до 5.000$ - 2%
-// Від 5.000$ до 10.000$ - 1,5%
-//   Від 10.000$ - 1 %
-
-// function Comission() {
-//   if (amount < 1000) {
-//    const comissionAmount = price * 0.025;
-//    const finalSum = sum + comissionAmount;
-//   } else if (amount) {
-//     price * 0.02;
-//   } else if (amount) {
-//     price * 0.015;
-//   } else {
-//     price * 0.01;
-//   }
-// }
-
-// const API_KEY = 'b9474da3d7b64496bd5be9a5519204ba';
 
 export function CurrencyConverter() {
   const [fromCurrency, setFromCurrency] = useState('BTC');
