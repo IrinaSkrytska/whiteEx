5b2fdf845c0555a1f82936ae4415cc239be72ad9
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-01-30 01:16:35 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -19,6 +19,13 @@ export default function ShowModal({ show, handleClose, handleSubmit }) {
     tg.sendData(JSON.stringify(data));
   }, [name, phone, email]);
 
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    };
+  }, [onSendData]);
+
   const handleChange = event => {
     const { name, value } = event.target;
 
