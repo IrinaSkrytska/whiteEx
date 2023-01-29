1522b368581016289e4f23cc0724deac322e4aa5
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-01-29 23:41:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,14 @@
 import CurrencyConverter from './CurrencyConverter/CurrencyConverter';
+import { useEffect } from 'react';
+import { useTelegram } from './hooks/useTelegram';
 
 export const App = () => {
+  const { onToggleButton, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
     <>
       <CurrencyConverter />
