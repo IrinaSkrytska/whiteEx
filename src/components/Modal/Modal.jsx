d9b3859e1c7fb0a07b5caf0bc0017e88ac0ecb05
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-01-31 01:30:57 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -56,64 +56,67 @@ export default function ShowModal({ show, handleClose, handleSubmit }) {
         <Modal.Header></Modal.Header>
         <Modal.Body className={css.modal__body}>
           <div className={css.modal__items}>
-            <ul className={css.items}>
-              <li className={css.modal__item}>
-                <h2 className={css.modal__item__title}>Имя</h2>
-                <input
-                  type="text"
-                  name="name"
-                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                  required
-                  value={name}
-                  className={css.modal__input}
-                  onChange={handleChange}
-                />
-              </li>
-              <li className={css.modal__item}>
-                <h2 className={css.modal__item__title}>
-                  Телеграмм/контактный номер
-                </h2>
-                <input
-                  type="text"
-                  name="phone"
-                  required
-                  value={phone}
-                  className={css.modal__input}
-                  onChange={handleChange}
-                />
-              </li>
-              <li className={css.modal__item}>
-                <h2 className={css.modal__item__title}>E-mail</h2>
+            <form action="https://app.form2chat.io/f/8eaa3f98" method="POST">
+              <ul className={css.items}>
+                <li className={css.modal__item}>
+                  <h2 className={css.modal__item__title}>Имя</h2>
 
-                <input
-                  type="email"
-                  name="email"
-                  required
-                  value={email}
-                  className={css.modal__input}
-                  onChange={handleChange}
-                />
-              </li>
-              <li className={css.modal__item}>
-                <Button
-                  type="submit"
-                  onClick={handleSubmit}
-                  className={css.button}
-                >
-                  Отправить
-                </Button>
-              </li>
-              <li className={css.modal__item}>
-                <Button
-                  type="button"
-                  onClick={handleClose}
-                  className={css.button__close}
-                >
-                  Закрыть
-                </Button>
-              </li>
-            </ul>
+                  <input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                    value={name}
+                    className={css.modal__input}
+                    onChange={handleChange}
+                  />
+                </li>
+                <li className={css.modal__item}>
+                  <h2 className={css.modal__item__title}>
+                    Телеграмм/контактный номер
+                  </h2>
+                  <input
+                    type="text"
+                    name="phone"
+                    required
+                    value={phone}
+                    className={css.modal__input}
+                    onChange={handleChange}
+                  />
+                </li>
+                <li className={css.modal__item}>
+                  <h2 className={css.modal__item__title}>E-mail</h2>
+
+                  <input
+                    type="email"
+                    name="email"
+                    required
+                    value={email}
+                    className={css.modal__input}
+                    onChange={handleChange}
+                  />
+                </li>
+                <li className={css.modal__item}>
+                  <Button
+                    type="submit"
+                    onClick={handleSubmit}
+                    className={css.button}
+                  >
+                    Отправить
+                  </Button>
+                </li>
+                <li className={css.modal__item}>
+                  <Button
+                    type="button"
+                    onClick={handleClose}
+                    className={css.button__close}
+                  >
+                    Закрыть
+                  </Button>
+                </li>
+              </ul>
+            </form>
           </div>
         </Modal.Body>
       </Modal>
