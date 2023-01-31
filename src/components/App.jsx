3c2e062df341b78ad3adc43e936ad61e89571a63
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-01-31 21:12:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
 import CurrencyConverter from './CurrencyConverter/CurrencyConverter';
 import { useEffect } from 'react';
-import { useTelegram } from '../hooks/useTelegram';
+
 import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
-  const { tg } = useTelegram();
-
-  useEffect(() => {
-    tg.ready();
-  }, [tg]);
-
   return (
     <Routes>
       <Route path="/" element={<CurrencyConverter />} />
