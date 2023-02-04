0c84403218b2db8ab18a457434f98f9321e2e32b
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-02-04 20:17:47 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -39,249 +39,249 @@ const cryptoCurrencies = [
 ];
 
 export default function ShowModal({ show, handleClose, handleSubmit, value }) {
-  const [fromCurrency] = useState('bitcoin');
-  const [toCurrency] = useState('usd');
-  const [fromPrice, setFromPrice] = useState('');
-  const [toPrice, setToPrice] = useState('');
-  const [rates, setRates] = useState();
-  const [setComission] = useState('');
-  const [setComissionFee] = useState('');
+  // const [fromCurrency] = useState('bitcoin');
+  // const [toCurrency] = useState('usd');
+  // const [fromPrice, setFromPrice] = useState('');
+  // const [toPrice, setToPrice] = useState('');
+  // const [rates, setRates] = useState();
+  // const [setComission] = useState('');
+  // const [setComissionFee] = useState('');
 
-  useEffect(() => {
-    setFromPrice('');
-    setToPrice('');
+  // useEffect(() => {
+  //   setFromPrice('');
+  //   setToPrice('');
 
-    if (fromCurrency === 'bitcoin' && toCurrency === 'usd') {
-      fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT').then(
-        res =>
-          res.json().then(json => {
-            console.log(json.price);
-            const roundedRates = Math.round(json.price);
-            setRates(roundedRates);
-          })
-      );
-    }
-    if (fromCurrency === 'bitcoin' && toCurrency === 'EUR') {
-      fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCEUR').then(res =>
-        res.json().then(json => {
-          console.log(json.price);
-          const roundedRates = Math.round(json.price);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'bitcoin' && toCurrency === 'pln') {
-      fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCPLN').then(res =>
-        res.json().then(json => {
-          console.log(json.price);
-          const roundedRates = Math.round(json.price);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'usd' && toCurrency === 'bitcoin') {
-      fetch(`https://api.exchangerate.host/convert?from=USD&to=BTC`).then(res =>
-        res.json().then(json => {
-          console.log(json.result);
-          const roundedRates = Math.round(json.result);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'EUR' && toCurrency === 'bitcoin') {
-      fetch(
-        `https://api.exchangerate.host/convert?from=${fromCurrency}&to=BTC`
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.result);
-          setRates(json.result);
-        })
-      );
-    }
-    if (fromCurrency === 'pln' && toCurrency === 'bitcoin') {
-      fetch(`https://api.exchangerate.host/convert?from=PLN&to=BTC`).then(res =>
-        res.json().then(json => {
-          console.log(json.result);
+  //   if (fromCurrency === 'bitcoin' && toCurrency === 'usd') {
+  //     fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT').then(
+  //       res =>
+  //         res.json().then(json => {
+  //           console.log(json.price);
+  //           const roundedRates = Math.round(json.price);
+  //           setRates(roundedRates);
+  //         })
+  //     );
+  //   }
+  //   if (fromCurrency === 'bitcoin' && toCurrency === 'EUR') {
+  //     fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCEUR').then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.price);
+  //         const roundedRates = Math.round(json.price);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'bitcoin' && toCurrency === 'pln') {
+  //     fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCPLN').then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.price);
+  //         const roundedRates = Math.round(json.price);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'usd' && toCurrency === 'bitcoin') {
+  //     fetch(`https://api.exchangerate.host/convert?from=USD&to=BTC`).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.result);
+  //         const roundedRates = Math.round(json.result);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'EUR' && toCurrency === 'bitcoin') {
+  //     fetch(
+  //       `https://api.exchangerate.host/convert?from=${fromCurrency}&to=BTC`
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.result);
+  //         setRates(json.result);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'pln' && toCurrency === 'bitcoin') {
+  //     fetch(`https://api.exchangerate.host/convert?from=PLN&to=BTC`).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.result);
 
-          setRates(json.result);
-        })
-      );
-    }
-    if (fromCurrency === 'ethereum' && toCurrency === 'usd') {
-      fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT').then(
-        res =>
-          res.json().then(json => {
-            console.log(json.price);
-            const roundedRates = Math.round(json.price);
-            setRates(roundedRates);
-          })
-      );
-    }
-    if (fromCurrency === 'ethereum' && toCurrency === 'EUR') {
-      fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHEUR').then(res =>
-        res.json().then(json => {
-          console.log(json.price);
-          const roundedRates = Math.round(json.price);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'ethereum' && toCurrency === 'pln') {
-      fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHPLN').then(res =>
-        res.json().then(json => {
-          console.log(json.price);
-          const roundedRates = Math.round(json.price);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'usd' && toCurrency === 'ethereum') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=ETH'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.ETH);
-          setRates(json.ETH);
-        })
-      );
-    }
-    if (fromCurrency === 'EUR' && toCurrency === 'ethereum') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=ETH'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.ETH);
-          setRates(json.ETH);
-        })
-      );
-    }
-    if (fromCurrency === 'pln' && toCurrency === 'ethereum') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=ETH'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.ETH);
-          setRates(json.ETH);
-        })
-      );
-    }
-    if (fromCurrency === 'tether' && toCurrency === 'usd') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=USD'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.USD);
-          const roundedRates = Math.round(json.USD);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'tether' && toCurrency === 'EUR') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=EUR'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.EUR);
-          const roundedRates = Math.round(json.EUR);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'tether' && toCurrency === 'pln') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=PLN'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.PLN);
-          const roundedRates = Math.round(json.PLN);
-          setRates(roundedRates);
-        })
-      );
-    }
-    if (fromCurrency === 'usd' && toCurrency === 'tether') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=USDT'
-      ).then(res =>
-        res.json().then(json => {
-          setRates(json.USDT);
-          console.log(json.USDT);
-        })
-      );
-    }
-    if (fromCurrency === 'EUR' && toCurrency === 'tether') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=USDT'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.USDT);
-          setRates(json.USDT);
-        })
-      );
-    }
-    if (fromCurrency === 'pln' && toCurrency === 'tether') {
-      fetch(
-        'https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=USDT'
-      ).then(res =>
-        res.json().then(json => {
-          console.log(json.USDT);
+  //         setRates(json.result);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'ethereum' && toCurrency === 'usd') {
+  //     fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT').then(
+  //       res =>
+  //         res.json().then(json => {
+  //           console.log(json.price);
+  //           const roundedRates = Math.round(json.price);
+  //           setRates(roundedRates);
+  //         })
+  //     );
+  //   }
+  //   if (fromCurrency === 'ethereum' && toCurrency === 'EUR') {
+  //     fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHEUR').then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.price);
+  //         const roundedRates = Math.round(json.price);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'ethereum' && toCurrency === 'pln') {
+  //     fetch('https://api.binance.com/api/v3/avgPrice?symbol=ETHPLN').then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.price);
+  //         const roundedRates = Math.round(json.price);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'usd' && toCurrency === 'ethereum') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=ETH'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.ETH);
+  //         setRates(json.ETH);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'EUR' && toCurrency === 'ethereum') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=ETH'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.ETH);
+  //         setRates(json.ETH);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'pln' && toCurrency === 'ethereum') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=ETH'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.ETH);
+  //         setRates(json.ETH);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'tether' && toCurrency === 'usd') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=USD'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.USD);
+  //         const roundedRates = Math.round(json.USD);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'tether' && toCurrency === 'EUR') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=EUR'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.EUR);
+  //         const roundedRates = Math.round(json.EUR);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'tether' && toCurrency === 'pln') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=PLN'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.PLN);
+  //         const roundedRates = Math.round(json.PLN);
+  //         setRates(roundedRates);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'usd' && toCurrency === 'tether') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=USDT'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         setRates(json.USDT);
+  //         console.log(json.USDT);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'EUR' && toCurrency === 'tether') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=USDT'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.USDT);
+  //         setRates(json.USDT);
+  //       })
+  //     );
+  //   }
+  //   if (fromCurrency === 'pln' && toCurrency === 'tether') {
+  //     fetch(
+  //       'https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=USDT'
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.USDT);
 
-          setRates(json.USDT);
-        })
-      );
-    }
-    if (
-      fromCurrency === 'bitcoin' ||
-      fromCurrency === 'ethereum' ||
-      fromCurrency === 'tether'
-    ) {
-      fetch(`https://api.coincap.io/v2/rates/${fromCurrency}`).then(res =>
-        res.json().then(json => {
-          console.log(json.data.rateUsd);
-          const roundedComission = Math.round(json.data.rateUsd);
-          setComission(roundedComission);
-        })
-      );
-    }
-    if (toCurrency === 'bitcoin') {
-      fetch(
-        `https://api.exchangerate.host/convert?from=${fromCurrency}&to=BTC`
-      ).then(res =>
-        res.json().then(json => {
-          setComission(json.result);
-          console.log(json.result);
-        })
-      );
-    }
-    if (
-      fromCurrency === 'usd' ||
-      fromCurrency === 'EUR' ||
-      (fromCurrency === 'pln' && toCurrency === 'ethereum')
-    ) {
-      fetch(
-        `https://min-api.cryptocompare.com/data/price?fsym=${fromCurrency}&tsyms=ETH`
-      ).then(res =>
-        res.json().then(json => {
-          setComission(json.ETH);
-          console.log(json.ETH);
-        })
-      );
-    }
-    if (
-      fromCurrency === 'usd' ||
-      fromCurrency === 'EUR' ||
-      (fromCurrency === 'pln' && toCurrency === 'tether')
-    ) {
-      fetch(
-        `https://min-api.cryptocompare.com/data/price?fsym=${fromCurrency}&tsyms=USDT`
-      ).then(res =>
-        res.json().then(json => {
-          setComission(json.USDT);
-          console.log(json.USDT);
-        })
-      );
-    }
-    setComissionFee('');
-  }, [fromCurrency, toCurrency, rates, setComissionFee, setComission]);
+  //         setRates(json.USDT);
+  //       })
+  //     );
+  //   }
+  //   if (
+  //     fromCurrency === 'bitcoin' ||
+  //     fromCurrency === 'ethereum' ||
+  //     fromCurrency === 'tether'
+  //   ) {
+  //     fetch(`https://api.coincap.io/v2/rates/${fromCurrency}`).then(res =>
+  //       res.json().then(json => {
+  //         console.log(json.data.rateUsd);
+  //         const roundedComission = Math.round(json.data.rateUsd);
+  //         setComission(roundedComission);
+  //       })
+  //     );
+  //   }
+  //   if (toCurrency === 'bitcoin') {
+  //     fetch(
+  //       `https://api.exchangerate.host/convert?from=${fromCurrency}&to=BTC`
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         setComission(json.result);
+  //         console.log(json.result);
+  //       })
+  //     );
+  //   }
+  //   if (
+  //     fromCurrency === 'usd' ||
+  //     fromCurrency === 'EUR' ||
+  //     (fromCurrency === 'pln' && toCurrency === 'ethereum')
+  //   ) {
+  //     fetch(
+  //       `https://min-api.cryptocompare.com/data/price?fsym=${fromCurrency}&tsyms=ETH`
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         setComission(json.ETH);
+  //         console.log(json.ETH);
+  //       })
+  //     );
+  //   }
+  //   if (
+  //     fromCurrency === 'usd' ||
+  //     fromCurrency === 'EUR' ||
+  //     (fromCurrency === 'pln' && toCurrency === 'tether')
+  //   ) {
+  //     fetch(
+  //       `https://min-api.cryptocompare.com/data/price?fsym=${fromCurrency}&tsyms=USDT`
+  //     ).then(res =>
+  //       res.json().then(json => {
+  //         setComission(json.USDT);
+  //         console.log(json.USDT);
+  //       })
+  //     );
+  //   }
+  //   setComissionFee('');
+  // }, [fromCurrency, toCurrency, rates, setComissionFee, setComission]);
 
   // const onChangeFromPrice = value => {
   //   const priceWithComission = comission * value;
@@ -386,7 +386,7 @@ export default function ShowModal({ show, handleClose, handleSubmit, value }) {
                   <select>
                     <options>
                       {cryptoCurrencies.map(({ key, value, img, name }) => (
-                        <li key={value} onClick={() => fromPrice(value)}>
+                        <li key={value}>
                           <p className={css.cryptocurrency__type}>
                             <span className={css.currency__img}>
                               <img src={img} width="36" height="36" alt="" />
@@ -403,7 +403,7 @@ export default function ShowModal({ show, handleClose, handleSubmit, value }) {
                   <select>
                     <options>
                       {fiatCurrencies.map(({ key, value, img, name }) => (
-                        <li key={value} onClick={() => toPrice(value)}>
+                        <li key={value}>
                           <p className={css.cryptocurrency__type}>
                             <span className={css.currency__img}>
                               <img src={img} width="36" height="36" alt="" />
